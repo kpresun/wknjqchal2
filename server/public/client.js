c5e4f7f3cdf7855e96651ccf969fd83005519da6
Kong Yang <kpyang2414@gmail.com>
2021-06-27 23:21:59 -0500

    calculator not pulling inputs in

--- server/public/client.js
+++ server/public/client.js
@@ -2,24 +2,42 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('js ready');
-    $('.top').on('click', '.operations', function(){
+    $('.top').on('click', '.results', function(){
         operatorSwitch();
     })
 
 }
 
+function operatorSwitch() {
 $.ajax({
     // type
     method: 'POST',
-    url: '/operatoraction'
-    // if data needs to be an object
-    // data: {item: abc, quantity: 123} becomes req.body on the server
-})
-// good path
+    url: '/operatorAction',
+    data: {
+        input1: $('#num1').val(),
+        input2: $('#num2').val(),
+        clickedOp: $('button').click(function(){
+            let chosenBtn = $(this).val();
+            return chosenBtn;
+        })
+    } // data becomes req.body on the server
+}) // good path
 .then(function(response){
     console.log('sent operator', response);
-})
-// error path
+}) // error path
 .catch(function(response) {
     console.log('operator failed', err);
+})
+
+}
+
+$.ajax({
+    method: 'GET',
+    url: '/operationResults'
+})
+.then(function(response) {
+    console.log('results made it back');
+})
+.catch(function(response){
+    console.log('operation failed');
 })
--- server/server.js
+++ server/server.js
@@ -6,12 +6,48 @@ app.use(express.static('server/public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-
 app.listen(PORT, () => {
     console.log(`listening on port; ${PORT}`);
 })
-// required codes above, below server side functions.
 
-app.post('/operatoraction', (req,res) => {
+//------------------------------------------------------//
+// required codes above, below server side calls.
+//------------------------------------------------------//
+
+app.post('/operatorAction', (req,res) => {
     console.log('operators made it to server');
+    let data = req.body;
+    console.log(req.body);
+    res.send(req.body);
+}
 });
+
+app.get('/operationResults', (req,res) => {
+    res.send(calculateMath)
+})
+
+//------------------------------------------------------//
+// required calls above, below server side functions.
+//------------------------------------------------------//
+
+
+let opBtn = data[2].clickedOp;
+
+function calculateMath() {
+    switch (opBtn) {
+        case '+':
+            return $('#num1').val() + ('#num2').val();
+            break;
+        case '-':
+            return $('#num1').val() - ('#num2').val();
+            break;
+        case '*':
+            return $('#num1').val() * ('#num2').val();
+            break;
+        case '/':
+           return  $('#num1').val() / ('#num2').val();
+            break;
+            
+        default:
+            break;
+    }
